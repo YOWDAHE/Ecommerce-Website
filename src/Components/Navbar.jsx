6a1fbd89d6f890d42a488eaa636fdd466c773385
Yodahe <ketemayodahe@gmail.com>
2023-04-02 22:11:10 +0300

    navBar finish

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,20 @@
-import CartIcon from "../icon";
+import {CartIcon, UserIcon} from "../icon";
 
 
 const Navbar = () => {
     return (
-        <div className=" bg-gray-900 flex h-10 my-1 mx-2 rounded-lg p-3 items-center text-white">
-            <div>
+        <div className=" bg-gray-300 flex h-15 my-1 mx-2 rounded-lg p-2 items-center text-gray-900">
+            <div className="mr-auto ml-1">
                 Company
             </div>
-            <div>
-                <CartIcon/>
+            <div className="flex">
+                <div className="pr-3 relative hover:cursor-pointer">
+                    <div className="absolute bg-gray-500 bg rounded-full w-6 text-center text-sm right-1 top-0 bg-opacity-80 text-white">0</div>
+                    <CartIcon/>
+                </div>
+                <div>
+                    <UserIcon/>
+                </div>
             </div>
         </div>
     )
