f93d94a4aa4389eec0f9d343d67cb954d5f5e2d3
Yodahe <ketemayodahe@gmail.com>
2023-04-02 18:13:20 +0300

    navbar start

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import Navbar from "./Navbar";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  )
 }
 export default App;
+
