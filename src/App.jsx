eebec02ad6e9f89371980c4780a2dbd5af1cf1fc
Yodahe <ketemayodahe@gmail.com>
2023-04-03 14:11:24 +0300

    main ui complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Navbar from "./Components/Navbar";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Cart from "./Components/Cart";
+import MainComp from "./Components/MainComponent";
+import { calculateTotals } from "./features/cartSlice";
 function App() {
   const { isShowing } = useSelector((state) => state.cartShow);
   return (
     <div className="app">
       {isShowing && <Navbar />}
       {!isShowing && <Cart />}
+      {isShowing && <MainComp/>}
     </div>
   )
 }
