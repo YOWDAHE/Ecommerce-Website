fd2b4a863e0b89ff07f2eefeffb0b7ff31b63f4d
Yodahe <ketemayodahe@gmail.com>
2023-04-03 14:17:40 +0300

    cart amount and main page link complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,16 @@ import { useSelector, useDispatch } from "react-redux";
 import Cart from "./Components/Cart";
 import MainComp from "./Components/MainComponent";
 import { calculateTotals } from "./features/cartSlice";
+import { useEffect } from "react";
 function App() {
+  const dispatch = useDispatch();
   const { isShowing } = useSelector((state) => state.cartShow);
+  const { cartItems } = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  },[cartItems])
+
   return (
     <div className="app">
       {isShowing && <Navbar />}
