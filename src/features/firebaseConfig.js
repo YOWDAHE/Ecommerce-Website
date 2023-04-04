11c85310367223b7130f1e5784abe369e25a5ee4
Yodahe <ketemayodahe@gmail.com>
2023-04-04 23:40:35 +0300

    Before Authentication

--- src/features/firebaseConfig.js
+++ src/features/firebaseConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
 const firebaseConfig = {
     apiKey: "AIzaSyDyvzlJNz3jaRhC61-dQTfBmcpAHEuSsPg",
     authDomain: "ecommerce-site-651d5.firebaseapp.com",
@@ -12,3 +12,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
