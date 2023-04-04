a7bcb83bed89e77b656cc2c326eecac6047de8df
Yodahe <ketemayodahe@gmail.com>
2023-04-04 14:40:31 +0300

    firebase config setup

---
+++ src/features/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDyvzlJNz3jaRhC61-dQTfBmcpAHEuSsPg",
+    authDomain: "ecommerce-site-651d5.firebaseapp.com",
+    projectId: "ecommerce-site-651d5",
+    storageBucket: "ecommerce-site-651d5.appspot.com",
+    messagingSenderId: "566207411177",
+    appId: "1:566207411177:web:8d9283a36111006a759178",
+    measurementId: "G-68Y4692V6Z"
+};
+
+const app = initializeApp(firebaseConfig);
